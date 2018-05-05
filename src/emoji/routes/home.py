754389ac598533f5ea538849465908763090d6fe
Pine Mizune <pinemz@gmail.com>
2018-05-06 01:40:47 +0900

    fix(home.py): redirect new domain

--- src/emoji/routes/home.py
+++ src/emoji/routes/home.py
@@ -9,11 +9,7 @@ from emoji import app
 
 @app.route('/')
 def home():
-    if app.config['DEBUG']:
-        epoch_f = datetime.now().timestamp()
-        args = { 'js_timestamp': hashlib.md5(str(epoch_f).encode('utf-8')).hexdigest() }
-        return render_template('index.j2', **args)
-    return render_template('index.j2')
+    return redirect('https://emoji-gen.ninja', code=301)
 
 @app.route('/sitemap.xml')
 def sitemap_xml():
